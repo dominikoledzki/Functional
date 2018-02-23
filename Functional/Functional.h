bbfff02c20f75362d5fe2a5560928627539f481a
Dominik Olędzki <dominik.oledzki@gmail.com>
2018-02-23 20:34:37 +0100

    Initial Commit

---
+++ Functional/Functional.h
@@ -0,0 +1,19 @@
+//
+//  Functional.h
+//  Functional
+//
+//  Created by Dominik Personal on 23/02/2018.
+//  Copyright © 2018 Dominik Olędzki. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Functional.
+FOUNDATION_EXPORT double FunctionalVersionNumber;
+
+//! Project version string for Functional.
+FOUNDATION_EXPORT const unsigned char FunctionalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Functional/PublicHeader.h>
+
+
